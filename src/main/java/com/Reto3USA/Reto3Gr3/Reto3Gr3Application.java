a1e28b2fc534732060acf56f7ddc1f3385ad7660
adminjunior <adminjunior@debian-JR.olimpica.com.co>
2021-10-20 07:57:06 -0500

    primeros archivos

---
+++ src/main/java/com/Reto3USA/Reto3Gr3/Reto3Gr3Application.java
@@ -0,0 +1,25 @@
+package com.Reto3USA.Reto3Gr3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+
+@EntityScan(basePackages = {"com.Reto3USA.Reto3Gr3"})
+@SpringBootApplication
+
+
+public class Reto3Gr3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Reto3Gr3Application.class, args);
+                
+
+}
+
+}
+        
+        
+        
+        
+       
