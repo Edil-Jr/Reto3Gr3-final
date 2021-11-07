e202048cc7ed024d279e0cfe79b810c3e66793d2
Edilberto <edilberto.holguin.mt@correo.usa.edu.co>
2021-11-07 10:40:50 -0500

    Se arregla boton borrar

--- js/borrar.js
+++ js/borrar.js
@@ -15,7 +15,7 @@ function borrarRegistro(llaveRegistro) {
     $.ajax({
         // la URL para la petición (url: "url al recurso o endpoint")
         //url: "http://localhost:8080/api/Cabin/"+llaveRegistro,
-        url: "http://168.138.68.19/:8080/api/Cabin/"+llaveRegistro,
+        url: "http://168.138.68.19:8080/api/Cabin/"+llaveRegistro,
 
         // la información a enviar
         // (también es posible utilizar una cadena de datos)
